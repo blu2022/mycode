bca77cc0175eed5867c733fef2e0cb9efad4c810
blu2022 <blu2022@users.noreply.github.com>
2022-04-16 01:33:12 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#create a list of 5 animals with 3 letter names and have your program output them to the screen.
+
+
+def main():
+
+    #create a list
+    alist1 = ["Fox ", "Fly ", "Ant ", "Bee ", "Cod "]
+
+    print((alist1[0]) + (alist1[1]) + (alist1[2]) + (alist1[3]) + (alist1[4]))
+
+main()
+
