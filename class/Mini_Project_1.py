8fcbe6b8504b4a720d9d2f692a206f51e625dc01
blu2022 <blu2022@users.noreply.github.com>
2022-04-20 21:33:02 +0000

    420 work

--- class/Mini_Project_1.py
+++ class/Mini_Project_1.py
@@ -8,7 +8,7 @@ round = 0           # integer round initiated to 0
 
 while True:        # sets up an infinite loop condition
     round += 1     # increase the round counter
-    print("Will Bai accept the relocation?\n")
+    print("Will Bai accept the relocation from a new job?\n")
 
     # How is the income tax rate level in your state as of Jan 2022?
     # https://taxfoundation.org/publications/state-individual-income-tax-rates-and-brackets/
@@ -27,7 +27,7 @@ while True:        # sets up an infinite loop condition
     print("\n"*2)
     # Type the median home price 
     # https://worldpopulationreview.com/state-rankings/median-home-price-by-state
-    Median_Home_Price= input("Please input median home price (\$):\n>")
+    Median_Home_Price= input("Please input median home price ($):\n>")
 
 #   if function
     if float(Statetaxburden) <= 13 and float(Median_Home_Price) <= 300000: #logic to check if given data meet the conditions
@@ -43,10 +43,10 @@ while True:        # sets up an infinite loop condition
             print('This is an invalid answer. Please restart the script.')
             break             
     elif round == 3:        # logic to ensure round has not yet reached 3
-        print(f'Either tax burden of {float(Statetaxburden)} or home price of {float(Median_Home_Price)} is too high!\nBai will refuse to relocate!\n\n Sorry, you run out of chances. Please restart the script if you want.\n\n')
+        print(f'Either tax burden of {float(Statetaxburden)} % or home price of {float(Median_Home_Price)} is too high!\nBai will refuse to relocate!\n\n Sorry, you run out of chances. Please restart the script if you want.\n\n')
         break             
     else:                 # if answer was wrong, and round is not yet equal to 3
-        print(f'Either tax burden of {float(Statetaxburden)} or home price of {float(Median_Home_Price)} is too high!\nBai will refuse to relocate!\n\n')
+        print(f'Either tax burden of {float(Statetaxburden)}% or home price of {float(Median_Home_Price)} is too high!\nBai will refuse to relocate!\n\n')
         answer2= input('Do you want to try again?(Y or N)\n>')
         answer2= answer2.upper()
         if answer2 == "N":
